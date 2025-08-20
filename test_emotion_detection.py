056f4cd6219d3ac2ec340745ef23477949d7e0b0
omers <omershect@users.noreply.github.com>
2025-08-20 11:15:21 +0300

    Create test_emotion_detection.py
    Add unit tests

---
+++ test_emotion_detection.py
@@ -0,0 +1,9 @@
+#Test file to run unit testing
+from emotion_detection import emotion_detector
+
+
+print(emotion_detector("I am glad this happened"))
+print(emotion_detector("I am really mad about this"))
+print(emotion_detector("I feel disgusted just hearing about this"))
+print(emotion_detector("I am so sad about this"))
+print(emotion_detector("I am really afraid that this will happen"))
